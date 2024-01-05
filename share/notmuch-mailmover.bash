9b636dee95ddb80bff5fe1a554843b4941e66f07
Michael Adler <therisen06@gmail.com>
2024-01-05 08:43:55 +0100

    chore: update generated files

--- share/notmuch-mailmover.bash
+++ share/notmuch-mailmover.bash
@@ -51,4 +51,8 @@ _notmuch-mailmover() {
     esac
 }
 
-complete -F _notmuch-mailmover -o nosort -o bashdefault -o default notmuch-mailmover
+if [[ "${BASH_VERSINFO[0]}" -eq 4 && "${BASH_VERSINFO[1]}" -ge 4 || "${BASH_VERSINFO[0]}" -gt 4 ]]; then
+    complete -F _notmuch-mailmover -o nosort -o bashdefault -o default notmuch-mailmover
+else
+    complete -F _notmuch-mailmover -o bashdefault -o default notmuch-mailmover
+fi
